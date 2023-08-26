f355cfd91fd3e3df820b83a1bc46879973bd886f
drellxor <tryakinan@gmail.com>
2023-08-26 12:21:16 +0600

    fixed issues with attachments

--- addons/product_attachment/models/product_attachment.py
+++ addons/product_attachment/models/product_attachment.py
@@ -4,7 +4,11 @@ from odoo import api, fields, models
 class ProductAttachment(models.Model):
     _name = 'product.attachment'
     _description = 'Product attachments'
+    _inherit = ['website.published.mixin']
 
     product_id = fields.Many2one('product.template', 'Product Template', required=True)
     attachment = fields.Binary('Attachment', required=True)
     filename = fields.Char('Filename')
+
+    def _default_is_published(self):
+        return True

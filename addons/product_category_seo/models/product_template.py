b9777843720a911ee60a94a9675da7de7e55a52a
drellxor <tryakinan@gmail.com>
2023-10-02 00:27:08 +0600

    fix

--- addons/product_category_seo/models/product_template.py
+++ addons/product_category_seo/models/product_template.py
@@ -11,10 +11,11 @@ class ProductTemplate(models.Model):
         manufacturer = self.public_categ_ids if self.public_categ_ids else None
         while manufacturer is not None and manufacturer.parent_id:
             manufacturer = manufacturer.parent_id
+        manufacturer_name = manufacturer.name if manufacturer else 'None'
 
         if not self.website_meta_title:
-            self.website_meta_title = f'{manufacturer.name} {self.name}'
+            self.website_meta_title = f'{manufacturer_name} {self.name}'
         if not self.website_meta_description:
-            self.website_meta_description = f'Order spares and service for {manufacturer.name} {self.name}'
+            self.website_meta_description = f'Order spares and service for {manufacturer_name} {self.name}'
         if not self.website_meta_keywords:
-            self.website_meta_keywords = f'{manufacturer.name}, {self.name}, spares, service, plant, machinery, worldwide, order, delivery'
+            self.website_meta_keywords = f'{manufacturer_name}, {self.name}, spares, service, plant, machinery, worldwide, order, delivery'

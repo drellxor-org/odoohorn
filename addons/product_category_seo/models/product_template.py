60d8804e1d0d427dcd88b55cc40c6896e524038a
drellxor <tryakinan@gmail.com>
2025-04-20 18:04:40 +0300

    updated bug with seo

--- addons/product_category_seo/models/product_template.py
+++ addons/product_category_seo/models/product_template.py
@@ -8,14 +8,19 @@ class ProductTemplate(models.Model):
     def init_record_metadata(self):
         super().init_record_metadata()
 
-        manufacturer = self.public_categ_ids if self.public_categ_ids else None
-        while manufacturer is not None and manufacturer.parent_id:
-            manufacturer = manufacturer.parent_id
-        manufacturer_name = manufacturer.name if manufacturer else 'None'
+        manufacturers = self.public_categ_ids if self.public_categ_ids else []
+        manufacturer_names = []
+        for manufacturer in manufacturers:
+            current_manufacturer = manufacturer
+            while current_manufacturer is not None and current_manufacturer.parent_id:
+                current_manufacturer = current_manufacturer.parent_id
+            if current_manufacturer:
+                manufacturer_names.append(current_manufacturer.name)
+        seo_manufacturer = ', '.join(manufacturer_names) or 'None'
 
         if not self.website_meta_title:
-            self.website_meta_title = f'{manufacturer_name} {self.name}'
+            self.website_meta_title = f'{seo_manufacturer} {self.name}'
         if not self.website_meta_description:
-            self.website_meta_description = f'Order spares and service for {manufacturer_name} {self.name}'
+            self.website_meta_description = f'Order spares and service for {seo_manufacturer} {self.name}'
         if not self.website_meta_keywords:
-            self.website_meta_keywords = f'{manufacturer_name}, {self.name}, spares, service, plant, machinery, worldwide, order, delivery'
+            self.website_meta_keywords = f'{seo_manufacturer}, {self.name}, spares, service, plant, machinery, worldwide, order, delivery'
